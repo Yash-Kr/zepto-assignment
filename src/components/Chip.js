b4e2bf1ed874b10f0cb2efec640ea2c52be9f0bc
Yash-Kr <yashk.ug19.cs@nitp.ac.in>
2024-01-16 04:07:22 +0530

    deployment configuration for github pages

--- src/components/Chip.js
+++ src/components/Chip.js
@@ -6,7 +6,7 @@ const Chip = ({ user = {}, on_delete = () => {}, className = "" }) => {
     <div
       className={`flex flex-row gap-2 py-[2px] px-[3px] items-center border-2 rounded-3xl shadow-md bg-white ${className}`}
     >
-      <img src="/images/p1.jpg" alt="p1" className="w-6 md:w-8 h-auto rounded-full" />
+      <img src="images/p1.jpg" alt="p1" className="w-6 md:w-8 h-auto rounded-full" />
       <span className="text-xs md:text-md">{user.name}</span>
       <span className="cursor-pointer" onClick={() => on_delete(user)}>
         {" "}
