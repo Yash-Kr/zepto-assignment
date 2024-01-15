901105b509212edf1cb9c35adf63b7570a064a96
Yash-Kr <yashk.ug19.cs@nitp.ac.in>
2024-01-16 04:10:05 +0530

    hotfix

--- src/components/Chip.js
+++ src/components/Chip.js
@@ -2,11 +2,19 @@ import React from "react";
 import { RxCross2 } from "react-icons/rx";
 
 const Chip = ({ user = {}, on_delete = () => {}, className = "" }) => {
+  const getProfileImageUrl = () => {
+    return `images/p${user.uniqueId}.jpg`;
+  };
+
   return (
     <div
       className={`flex flex-row gap-2 py-[2px] px-[3px] items-center border-2 rounded-3xl shadow-md bg-white ${className}`}
     >
-      <img src="images/p1.jpg" alt="p1" className="w-6 md:w-8 h-auto rounded-full" />
+      <img
+        src={getProfileImageUrl()}
+        alt="p1"
+        className="w-6 md:w-8 h-auto rounded-full"
+      />
       <span className="text-xs md:text-md">{user.name}</span>
       <span className="cursor-pointer" onClick={() => on_delete(user)}>
         {" "}
